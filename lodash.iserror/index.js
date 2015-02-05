1b799b838601fef7ee377b0cc494910b48be7f35
jdalton <john.david.dalton@gmail.com>
2015-02-04 20:53:49 -0800

    Bump to v3.1.1.

--- lodash.iserror/index.js
+++ lodash.iserror/index.js
@@ -1,5 +1,5 @@
 /**
- * lodash 3.1.0 (Custom Build) <https://lodash.com/>
+ * lodash 3.1.1 (Custom Build) <https://lodash.com/>
  * Build: `lodash modularize exports="npm" -o ./`
  * Copyright 2012-2016 The Dojo Foundation <http://dojofoundation.org/>
  * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
@@ -40,9 +40,8 @@ function isError(value) {
   if (!isObjectLike(value)) {
     return false;
   }
-  var Ctor = value.constructor;
   return (objectToString.call(value) == errorTag) ||
-    (typeof Ctor == 'function' && objectToString.call(Ctor.prototype) == errorTag);
+    (typeof value.message == 'string' && typeof value.name == 'string');
 }
 
 /**
